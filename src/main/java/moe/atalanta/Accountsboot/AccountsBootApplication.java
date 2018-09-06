444c8b73e6300fc636cea9585aaca4e89e2f157e
jim <jim@cit.com>
2018-09-06 13:22:03 +0800

    init

---
+++ src/main/java/moe/atalanta/Accountsboot/AccountsBootApplication.java
@@ -0,0 +1,12 @@
+package moe.atalanta.Accountsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsBootApplication.class, args);
+	}
+}
